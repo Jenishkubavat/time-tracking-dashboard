1c6ee5110f961d57f973e5777aea435f2fce9fb7
Jenishkubavat <dante2709201@gamail.com>
2022-02-25 14:58:24 +0530

    setup of config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'primary': {
+          'blue':'#5847eb',
+          'work':'#ff8c66',
+          'play':'#56c2e6',
+          'study':'#ff5c7c',
+          'exercise':'#4acf81',
+          'social':'#7536d3',
+          'care':'#f1c65b'
+        },
+        'neutral':{
+          'dark_blue':'#0f1424',
+          'blue':'#1c1f4a',
+          'desaturated_blue':'#6f76c8',
+          'pale_blue': '#bdc1ff'
+        }
+        
+      }
+    },
   },
   plugins: [],
 }
